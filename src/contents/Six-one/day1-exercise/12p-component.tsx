d59ebdf33284a98be12ff31649f5d66d4c8156ac
eunbeann <ds5bean23@gmail.com>
2023-12-11 01:17:07 +0900

    [ feat ] 6-12쪽

---
+++ src/contents/Six-one/day1-exercise/12p-component.tsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react';
+import { Box, Typography } from '@mui/material';
+
+import CorrectChecker from 'src/contents/common/correct-checker';
+import DivisionInput from 'src/contents/common/number-box';
+
+interface Component12PageProps {
+  problem: {
+    qId: number;
+    pass: boolean;
+    qNum: string;
+    lNum: number;
+    rNum: number;
+  };
+  isSolved: boolean;
+  handleCorrectChange: (qId: number, pass: boolean) => void;
+}
+
+export default function Component12Page(props: Component12PageProps) {
+  const { problem, isSolved, handleCorrectChange } = props;
+  const { qId, pass, qNum, lNum, rNum } = problem;
+
+  const [son, setSon] = useState(0);
+  const [mother, setMother] = useState(0);
+  const [isCorrect, setIsCorrect] = useState(false);
+
+  useEffect(() => {
+    if (son === lNum && mother === rNum) {
+      setIsCorrect(true);
+      handleCorrectChange(qId, true);
+    } else {
+      setIsCorrect(false);
+      handleCorrectChange(qId, false);
+    }
+  }, [isSolved, qId]);
+
+  return (
+    <Box display="flex" gap="0.2rem" alignItems="center">
+      {isSolved && <CorrectChecker isCorrect={isCorrect} />}
+      <Typography>{qNum}</Typography>
+      <Typography>
+        {lNum} ÷ {rNum} =
+      </Typography>
+      <DivisionInput
+        mother={mother}
+        son={son}
+        onChangeMother={e => setMother(Number(e.target.value))}
+        onChangeSon={e => setSon(Number(e.target.value))}
+      />
+    </Box>
+  );
+}
