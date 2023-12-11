986974179182a071949b774a1b3f6aada7f51064
eunbeann <ds5bean23@gmail.com>
2023-12-12 00:27:41 +0900

    [ etc ] 라우팅 수정

--- src/contents/Six-one/six-one.routes.ts
+++ src/contents/Six-one/six-one.routes.ts
@@ -16,24 +16,64 @@ import { createEliceExtRoutes } from '@elice/extcontent-utils';
 //
 
 export default createEliceExtRoutes([
+  {
+    path: 'quiz1-1-1',
+    component: import('./day1/p-10'),
+  },
+  {
+    path: 'quiz1-1-2',
+    component: import('./day1/p-11'),
+  },
+  {
+    path: 'quiz1-1-3',
+    component: import('./day1/p-12'),
+  },
+  {
+    path: 'quiz1-2-1',
+    component: import('./day2/p-13'),
+  },
+  {
+    path: 'quiz1-2-2',
+    component: import('./day2/p-14'),
+  },
+  {
+    path: 'quiz1-2-3',
+    component: import('./day2/p-15'),
+  },
   // {
-  //   path: 'example-exercise',
-  //   component: import('./{example-exercise}/{example-exercise}'),
+  //   path: 'day3-p16',
+  //   component: import('./day3/p-16'),
   // },
-  {
-    path: 'day1-example',
-    component: import('./day1-exercise/c-ex'),
-  },
-  {
-    path: 'day1-p10',
-    component: import('./day1-exercise/p-10'),
-  },
-  {
-    path: 'day1-p11',
-    component: import('./day1-exercise/p-11'),
-  },
-  {
-    path: 'day1-p12',
-    component: import('./day1-exercise/p-12'),
-  },
+  // {
+  //   path: 'day3-p17',
+  //   component: import('./day3/p-17'),
+  // },
+  // {
+  //   path: 'day3-p18',
+  //   component: import('./day3/p-18'),
+  // },
+  // {
+  //   path: 'day4-p19',
+  //   component: import('./day4/p-19'),
+  // },
+  // {
+  //   path: 'day4-p20',
+  //   component: import('./day4/p-20'),
+  // },
+  // {
+  //   path: 'day4-p21',
+  //   component: import('./day4/p-21'),
+  // },
+  // {
+  //   path: 'day5-p22',
+  //   component: import('./day5/p-22'),
+  // },
+  // {
+  //   path: 'day5-p23',
+  //   component: import('./day5/p-23'),
+  // },
+  // {
+  //   path: 'day5-p24',
+  //   component: import('./day5/p-24'),
+  // },
 ]);
