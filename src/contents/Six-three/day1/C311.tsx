d38526e84a26821d63f1ba31bcb30bb2cd03820b
simeunseo <rkdcjfehowl@naver.com>
2023-12-18 23:37:00 +0900

    [ feat ] P311 문제 매핑 및 뷰, 채점 로직 연결 완료

--- src/contents/Six-three/day1/C311.tsx
+++ src/contents/Six-three/day1/C311.tsx
@@ -1,14 +1,13 @@
 import { useEffect, useState } from 'react';
-import styled from '@emotion/styled';
-import { Apple, Person } from '@mui/icons-material';
-import { Box, Typography } from '@mui/material';
+import { Avatar, Box, Typography } from '@mui/material';
 
 import CorrectChecker from 'src/contents/common/correct-checker';
-import DivisionInput, { NumberInput } from 'src/contents/common/number-box';
-import { NumberUnderBar } from 'src/contents/common/number-underbar';
-import VisualFraction from 'src/contents/common/visual-fraction';
+import { NumberInput } from 'src/contents/common/number-box';
+import { TextUnderBar } from 'src/contents/common/text-underbar';
 
 import type { ProblemProp } from './P311';
+
+import ArrowRight from '../../assets//icon/arrowRight.svg';
 
 interface C311Props {
   problem: ProblemProp;
@@ -16,83 +15,82 @@ interface C311Props {
   handleCorrectChange: (qId: number, pass: boolean) => void;
 }
 
-type Fraction = [number | undefined, number | undefined];
-
 export default function C311(props: C311Props) {
   const { problem, isSolved, handleCorrectChange } = props;
-  const { qId, pass, qNum, top, bottom, firstType, secondType, thirdType } =
-    problem;
+  const { qId, pass, qNum, leftItem, rightItem, answer } = problem;
 
-  const [firstAnswer, setFirstAnswer] = useState<number | Fraction | undefined>(
-    firstType === 'integer' ? undefined : [undefined, undefined]
-  );
-  const [secondAnswer, setSecondAnswer] = useState<
-    number | Fraction | undefined
-  >(secondType === 'integer' ? undefined : [undefined, undefined]);
+  const [isCorrect, setIsCorrect] = useState(false);
 
-  const [thirdAnswer, setThirdAnswer] = useState<number | Fraction | undefined>(
-    thirdType === 'integer' ? undefined : [undefined, undefined]
-  );
+  const [firstInput, setFirstInput] = useState<string | undefined>(undefined);
+  const [secondInput, setSecondInput] = useState<string | undefined>(undefined);
+  const [thirdInput, setThirdInput] = useState<string | undefined>(undefined);
 
-  // const [isCorrect, setIsCorrect] = useState(false);
-
-  // useEffect(() => {
-  //   if (son === apples && mother === people) {
-  //     setIsCorrect(true);
-  //     handleCorrectChange(qId, true);
-  //   } else {
-  //     setIsCorrect(false);
-  //     handleCorrectChange(qId, false);
-  //   }
-  // }, [isSolved, qId]);
+  useEffect(() => {
+    if (
+      answer[0] === firstInput &&
+      answer[0] === secondInput &&
+      answer[1] === thirdInput
+    ) {
+      setIsCorrect(true);
+      handleCorrectChange(qId, true);
+    } else {
+      setIsCorrect(false);
+      handleCorrectChange(qId, false);
+    }
+  }, [isSolved, qId]);
 
   return (
     <Box>
-      <Box display="flex" alignItems="center" margin="1rem 2rem">
-        {/* {isSolved && <CorrectChecker isCorrect={isCorrect} />} */}
-        <Typography variant="h5" marginRight="1rem">
-          ①
-        </Typography>
-        <Typography variant="h5" fontWeight={600} marginRight="1rem">
-          {top} ÷ {bottom} =
-        </Typography>
-        {firstType === 'integer' ? (
-          <NumberUnderBar
-            value={Number(firstAnswer)}
-            onChange={e => {
-              setFirstAnswer(Number(e.target.value));
-            }}
-          />
-        ) : (
-          <FractionBox>
-            <NumberInput
-              value={Number(firstAnswer)}
-              onChange={e => {
-                setFirstAnswer(Number(e.target.value));
+      <Box display="flex" alignItems="start">
+        <Box display="flex" alignItems="center" position="relative">
+          {isSolved && <CorrectChecker isCorrect={isCorrect} />}
+          <Typography variant="h5" fontWeight={600} marginRight="1rem">
+            {qNum}
+          </Typography>
+        </Box>
+        <Box display="flex" flexDirection="column" gap="2rem">
+          <Box display="flex">
+            <Typography variant="h5" fontWeight={600}>
+              {leftItem} ÷ {rightItem} =
+            </Typography>
+            <TextUnderBar
+              width="4rem"
+              value={firstInput ? firstInput : ''}
+              onChange={e => setFirstInput(e.target.value)}
+            />
+          </Box>
+          <Box display="flex" marginLeft="-2.5rem">
+            <Avatar
+              src={ArrowRight}
+              variant="square"
+              style={{
+                width: '1.5rem',
+                height: 'max-content',
+                margin: '0rem 0.5rem',
               }}
             />
-            <Bar />
-            <NumberInput
-              value={Number(firstAnswer)}
-              onChange={e => {
-                setFirstAnswer(Number(e.target.value));
-              }}
+            <Typography variant="h5" fontWeight={600}>
+              {leftItem}는 {rightItem}의
+            </Typography>
+            <TextUnderBar
+              width="4rem"
+              value={secondInput ? secondInput : ''}
+              onChange={e => setSecondInput(e.target.value)}
             />
-          </FractionBox>
-        )}
+            <Typography variant="h5" fontWeight={600}>
+              배, {rightItem}는 {leftItem}의
+            </Typography>
+            <TextUnderBar
+              width="4rem"
+              value={thirdInput ? thirdInput : ''}
+              onChange={e => setThirdInput(e.target.value)}
+            />
+            <Typography variant="h5" fontWeight={600}>
+              배
+            </Typography>
+          </Box>
+        </Box>
       </Box>
     </Box>
   );
 }
-
-const FractionBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 0.2rem;
-`;
-
-const Bar = styled.div`
-  width: 2rem;
-  border-bottom: 0.08rem solid gray;
-`;
